099f7f7df9ee3c1517711706dc491e5607f17d4c
FredrickJ <jym352857@gmail.com>
2025-12-30 12:40:40 +0900

    Feed UI fix

---
+++ para/headerImages.ts
@@ -0,0 +1,12 @@
+export const HEADER_IMAGES = [
+    "para/1.jpg",
+    "para/2.jpg",
+    "para/3.jpg",
+    "para/4.jpg",
+    "para/5.jpg",
+    "para/6.jpg",
+    "para/7.jpg",
+    "para/8.jpg",
+    "para/9.jpg",
+    "para/10.jpg",
+];
